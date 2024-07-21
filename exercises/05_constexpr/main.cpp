91ba1c6d86642a3ca8d91744a46b8bc92406dacf
jmz0407 <614639031@qq.com>
2024-07-21 23:20:47 +0800

    Merge remote-tracking changes from origin/main

--- exercises/05_constexpr/main.cpp
+++ exercises/05_constexpr/main.cpp
@@ -1,24 +1,27 @@
 #include "../exercise.h"
 
-constexpr unsigned long long fibonacci(int i) {
-    switch (i) {
-        case 0:
-            return 0;
-        case 1:
-            return 1;
-        default:
-            return fibonacci(i - 1) + fibonacci(i - 2);
+const unsigned long long fibonacci(int i) {
+  if (i <= 1) {
+        return i;
     }
+    unsigned long long prev = 0;
+    unsigned long long curr = 1;
+    for (int n = 2; n <= i; ++n) {
+        unsigned long long next = prev + curr;
+        prev = curr;
+        curr = next;
+    }
+    return curr;
 }
 
 int main(int argc, char **argv) {
-    constexpr auto FIB20 = fibonacci(20);
+    const auto FIB20 = fibonacci(20);
     ASSERT(FIB20 == 6765, "fibonacci(20) should be 6765");
     std::cout << "fibonacci(20) = " << FIB20 << std::endl;
 
     // TODO: 观察错误信息，修改一处，使代码编译运行
-    constexpr auto ANS_N = 100;
-    constexpr auto ANS = fibonacci(ANS_N);
+    const auto ANS_N = 100;
+    const auto ANS = fibonacci(ANS_N);
     std::cout << "fibonacci(" << ANS_N << ") = " << ANS << std::endl;
 
     return 0;
--- exercises/10_method_const/main.cpp
+++ exercises/10_method_const/main.cpp
@@ -1,14 +1,18 @@
 #include "../exercise.h"
-
+#include <vector>
+using namespace std;
 struct Fibonacci {
-    int numbers[11];
+    vector<int> numbers;
+    Fibonacci(vector<int> num) : numbers(num.begin(), num.end()) {
+    }
     // TODO: 修改方法签名和实现，使测试通过
     int get(int i) {
+      return numbers[i];
     }
 };
 
 int main(int argc, char **argv) {
-    Fibonacci constexpr FIB{{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55}};
+    Fibonacci FIB(vector<int>{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55});
     ASSERT(FIB.get(10) == 55, "fibonacci(10) should be 55");
     std::cout << "fibonacci(10) = " << FIB.get(10) << std::endl;
     return 0;
